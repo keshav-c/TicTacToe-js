138209b344f6cf50161b3505d7eed3eaa9a1d608
keshav-c <keshav1985@gmail.com>
2020-07-16 19:35:37 +0530

    Show game result

--- app.js
+++ app.js
@@ -20,60 +20,59 @@ const board = ((() => {
   };
 
   const getRowWinner = () => {
-    let winner = -1;
+    let winner = [];
     if (state[0] === state[1] && state[1] === state[2] && state[0] !== '_') {
-      winner = 0;
+      winner = [0, 1, 2];
     } else if (state[3] === state[4] && state[4] === state[5] && state[3] !== '_') {
-      winner = 3;
+      winner = [3, 4, 5];
     } else if (state[6] === state[7] && state[7] === state[8] && state[6] !== '_') {
-      winner = 6;
+      winner = [6, 7, 8];
     }
     return winner;
   };
 
   const getColumnWinner = () => {
-    let winner = -1;
+    let winner = [];
     if (state[0] === state[3] && state[3] === state[6] && state[0] !== '_') {
-      winner = 0;
+      winner = [0, 3, 6];
     } else if (state[1] === state[4] && state[4] === state[7] && state[1] !== '_') {
-      winner = 1;
+      winner = [1, 4, 7];
     } else if (state[2] === state[5] && state[5] === state[8] && state[2] !== '_') {
-      winner = 2;
+      winner = [2, 5, 8];
     }
     return winner;
   };
 
   const getDiagonalWinner = () => {
-    let winner = -1;
+    let winner = [];
     if (state[0] === state[4] && state[4] === state[8] && state[0] !== '_') {
-      winner = 0;
+      winner = [0, 4, 8];
     } else if (state[2] === state[4] && state[4] === state[6] && state[2] !== '_') {
-      winner = 2;
+      winner = [2, 4, 6];
     }
     return winner;
   };
 
   const getWinner = () => {
-    const rw = getRowWinner();
-    if (rw >= 0) {
-      console.log(`${state[rw]} won`);
-      return;
+    let winningSequence;
+    winningSequence = getRowWinner();
+    if (winningSequence.length > 0) {
+      return winningSequence;
     }
-    const cw = getColumnWinner();
-    if (cw >= 0) {
-      console.log(`${state[cw]} won`);
-      return;
+    winningSequence = getColumnWinner();
+    if (winningSequence.length > 0) {
+      return winningSequence;
     }
-    const dw = getDiagonalWinner();
-    if (dw >= 0) {
-      console.log(`${state[dw]} won`);
-      return;
+    winningSequence = getDiagonalWinner();
+    if (winningSequence.length > 0) {
+      return winningSequence;
     }
     if (state.indexOf('_') > -1) {
-      console.log('Game ongoing');
-      return;
+      winningSequence = [];
+    } else {
+      winningSequence = ['D'];
     }
-    console.log('Draw');
+    return winningSequence;
   };
 
   const update = (row, col) => {
@@ -88,10 +87,15 @@ const board = ((() => {
 
   const getState = () => state;
 
-  return { getState, update, nextSymbol, getWinner };
+  return {
+    getState, update, nextSymbol, getWinner,
+  };
 })());
 
 const controller = ((() => {
+  let winner;
+  let winningSequence;
+
   const fillBoard = () => {
     let row = 0;
     let col = 0;
@@ -110,7 +114,26 @@ const controller = ((() => {
     }
   };
 
-  return { fillBoard };
+  const isGameOver = () => {
+    winningSequence = board.getWinner();
+    let finished = false;
+    if (winningSequence.length === 1) {
+      winner = 'D';
+      finished = true;
+    }
+    if (winningSequence.length === 3) {
+      winner = board.getState()[winningSequence[0]];
+      finished = true;
+    }
+    return finished;
+  };
+
+  const getWinner = () => winner;
+  const getWinningSequence = () => winningSequence;
+
+  return {
+    fillBoard, isGameOver, getWinner, getWinningSequence,
+  };
 })());
 
 controller.fillBoard();
@@ -125,5 +148,15 @@ gameBoard.addEventListener('click', event => {
   col = parseInt(col, 10) - 1;
   board.update(row, col);
   controller.fillBoard();
-  board.getWinner();
+  if (controller.isGameOver()) {
+    const resultBox = document.querySelector('#result');
+    const resultMessage = document.createElement('h2');
+    const winner = controller.getWinner();
+    if (winner === 'D') {
+      resultMessage.textContent = 'Draw';
+    } else {
+      resultMessage.textContent = `${winner} won the game!`;
+    }
+    resultBox.appendChild(resultMessage);
+  }
 });
