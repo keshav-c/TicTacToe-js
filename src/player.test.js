fcd91845c778fce4567776903526cd95f43f3df4
mbabali iryn <mbabaliiryn@gmail.com>
2020-08-26 16:05:41 +0300

    test reset player and increment score

--- src/player.test.js
+++ src/player.test.js
@@ -5,3 +5,18 @@ it('Creates new player with name and score 0', () => {
   expect(player.getName()).toBe('player1');
   expect(player.getScore()).toBe(0);
 });
+
+it('increaments score', () => {
+  const player = newPlayer('player1', 1);
+  player.incrementScore();
+  expect(player.getScore()).toBe(1);
+});
+
+it('resets player', () => {
+  const player = newPlayer('Iryn', 1);
+  player.incrementScore();
+  player.incrementScore();
+  player.resetPlayer('Keshav');
+  expect(player.getName()).toBe('Keshav');
+  expect(player.getScore()).toBe(0);
+});
